f3c089e60f8529b48ff5fd9065594ae11d1e72aa
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-29 19:06:25 -0300

    Criando o model Task.

--- task/models.py
+++ task/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    STATUS = (
+        ('doing', 'Doing'),
+        ('done', 'Done'),
+    )
+    title = models.CharField(max_length=265)
+    description = models.TextField()
+    done = models.CharField(max_length=9, choices=STATUS,)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
