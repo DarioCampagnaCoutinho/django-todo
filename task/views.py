70bccad8c74d4f3c34d5a335c2707634d57ecb9b
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-09-29 21:03:31 -0300

    Ajuste no css e criação da view detail.

--- task/views.py
+++ task/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Task
 
 
 def list(request):
     tasks = Task.objects.all()
     return render(request, 'task/list.html', {'tasks': tasks})
+
+
+def detail(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'task/detail.html', {'task': task})
