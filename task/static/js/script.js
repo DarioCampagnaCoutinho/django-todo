234bdc2f159fb598f31fb790004bb989b3333271
DarioCampagnaCoutinho <campagnacoutinho67@gmail.com>
2020-10-01 15:36:31 -0300

    Usando jquery para deletar.

---
+++ task/static/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var deleteBtn = $('.delete-btn');
+    $(deleteBtn).on('click', function(e){
+        e.preventDefault();
+        var delLink = $(this).attr('href');
+        var result = confirm('Quer deletar essa tarefa?')
+        if(result){
+            window.location.href = delLink;
+        }
+    });
+});
